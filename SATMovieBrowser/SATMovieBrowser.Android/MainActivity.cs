fe19f52ce88cda37129279337b81a69aa2d66ebe
Rajendra Mehra <razmehra@hotmail.com>
2019-09-26 16:56:16 +0530

    Apply Logos

--- SATMovieBrowser/SATMovieBrowser.Android/MainActivity.cs
+++ SATMovieBrowser/SATMovieBrowser.Android/MainActivity.cs
@@ -13,7 +13,7 @@ using Acr.UserDialogs;
 
 namespace SATMovieBrowser.Droid
 {
-    [Activity(Label = "SATMovieBrowser", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "TMBrowser", Icon = "@drawable/TMBLogo", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
